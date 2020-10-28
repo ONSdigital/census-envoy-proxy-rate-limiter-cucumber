924e3cb3f7686ed2c875cc64b9a30558c3931dca
andrewjohnysons <andrew.johnys@ext.ons.gov.uk>
2020-10-28 11:46:07 +0000

    more exception tweaks...

--- src/test/java/uk/gov/ons/ctp/integration/envoycuc/steps/LimitTestSteps.java
+++ src/test/java/uk/gov/ons/ctp/integration/envoycuc/steps/LimitTestSteps.java
@@ -14,6 +14,7 @@ import java.util.Collections;
 import java.util.List;
 import org.apache.commons.lang3.mutable.MutableInt;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.client.HttpClientErrorException;
 import uk.gov.ons.ctp.common.domain.CaseType;
 import uk.gov.ons.ctp.common.domain.UniquePropertyReferenceNumber;
@@ -132,10 +133,15 @@ public class LimitTestSteps {
                   r.getUprn(),
                   r.getTelNo());
         } catch (Exception ex) {
-          if (ex.getCause() instanceof HttpClientErrorException.TooManyRequests) {
+          if (ex.getCause() instanceof HttpClientErrorException) {
             isPass = false;
+            HttpClientErrorException cause = (HttpClientErrorException) ex.getCause();
+            if (cause.getStatusCode() != HttpStatus.TOO_MANY_REQUESTS) {
+              throw new RuntimeException(
+                  "Invalid status thrown for request: " + r.toString(), cause);
+            }
           } else {
-            throw new RuntimeException("Invalid status thrown for request: " + r.toString(), ex);
+            throw new RuntimeException("Unexpected error thrown for request: " + r.toString(), ex);
           }
         }
       }
